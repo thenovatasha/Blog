4ceaab579862ab9f167357366d0ac547f3b09540
ntasha <nova.tasha@student.unimelb.edu.au>
2024-07-13 00:31:10 +1000

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -9,6 +9,15 @@ app.get("/", function(req, res) {
 server.on("request", app);
 server.listen(3000, function() {console.log("server started on port 3000")});
 
+process.on('SIGINT', () => {
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutdownDB();
+    })
+})
+
 /** Begin Websocket */
 const WebsocketServer = require("ws").Server;
 
@@ -18,20 +27,50 @@ wss.on("connection", function connection(ws) {
     const numClients = wss.clients.size;
     console.log(`Clients connected`, numClients);
 
-    wss.boardcast(`Current visitors: ${numClients}`);
+    wss.broadcast(`Current visitors: ${numClients}`);
 
     if(ws.readyState === ws.OPEN) {
         ws.send("Welcome to my server");
     }
 
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
     ws.on('close', function close() {
-        wss.boardcast(`Current visitors: ${numClients}`);
+        const numClients = wss.clients.size;
+        wss.broadcast(`Current visitors: ${numClients}`);
         console.log("Client has disconnected");
     });
 });
 
-wss.boardcast = function broadcast(data) {
-    wss.clients.forEach(function each(clients) {
-        clients.send(data);
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
     });
-} 
+}
+
+/** End Websockets */
+/** Begin Database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `)
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    })
+}
+
+function shutdownDB() {
+    getCounts();
+    console.log("Shutting Down DB");
+    db.close();
+}
